a1339164765d2250ec887d799c1d81f7d1c0255c
jny0 <pjy100402@naver.com>
2023-06-27 11:17:09 +0900

    서브넷 추가

--- main.tf
+++ main.tf
@@ -30,3 +30,12 @@ resource "aws_vpc" "example" {
     Name = "example"
   }
 }
+
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "example_subnet"
+  }
+}
