68a88d9146239b86a3effffcf3dea947085e5d22
jny0 <pjy100402@naver.com>
2023-06-27 11:03:06 +0900

    vpc

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 terraform {
+  // 이 부분은 terraform cloud에서 설정한 workspace의 이름과 동일해야 함
+  // 이 부분은 terraform login 후에 사용가능함
   cloud {
     organization = "og1-jyp"
 
@@ -6,7 +8,6 @@ terraform {
       name = "ws-1"
     }
   }
-
 
   // 자바의 import 와 비슷함
   // aws 라이브러리 불러옴
@@ -18,8 +19,14 @@ terraform {
   }
 }
 
-
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-northeast-2"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example"
+  }
 }
